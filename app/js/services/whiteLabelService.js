9f8f4bb02b2414c04816a64fa37bf1dcd6b56243
Juergen May <j.may@accent.uk.com>
2016-09-29 14:57:41 +0100

    whiteLabelService.js - Bug fix after SourceTree sync issue

--- app/js/services/whiteLabelService.js
+++ app/js/services/whiteLabelService.js
@@ -4,14 +4,11 @@ four51.app.factory('WhiteLabel', function() {
 		{"key":"Cart","value":"Basket"},
 		{"key":"Cart Summary","value":"Basket Summary"},
 		{"key":"Cart Type","value":"Basket Type"},
-<<<<<<< HEAD
 		{"key":"Center","value":"Centre"},
 		{"key":"Favorite","value":"Favourite"},
 		{"key":"Favorites","value":"My Favourites"},
-=======
 		{"key":"Cost Center","value":"Cost Centre"},
 		{"key":"Shipping","value":"Delivery"},
->>>>>>> ecac757aaef882cfd5e914ac38b0ad1f2b9c6495
 		{"key":"Tax","value":"VAT"},
 		{"key":"State","value":"County"},
 		{"key":"User Information","value":"My Details"},
