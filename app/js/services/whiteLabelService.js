a66f9094bac8647275e1491bf5fa8d5b6a386add
Juergen May <j.may@accent.uk.com>
2016-09-29 11:07:14 +0100

    Fixed typos in UI

--- app/js/services/whiteLabelService.js
+++ app/js/services/whiteLabelService.js
@@ -1,16 +1,21 @@
 four51.app.factory('WhiteLabel', function() {
 	var replacements = [
+		{"key":"Canceled","value":"Cancelled"},
 		{"key":"Cart","value":"Basket"},
 		{"key":"Cart Summary","value":"Basket Summary"},
 		{"key":"Cart Type","value":"Basket Type"},
+<<<<<<< HEAD
+		{"key":"Center","value":"Centre"},
+		{"key":"Favorite","value":"Favourite"},
+		{"key":"Favorites","value":"My Favourites"},
+=======
 		{"key":"Cost Center","value":"Cost Centre"},
 		{"key":"Shipping","value":"Delivery"},
+>>>>>>> ecac757aaef882cfd5e914ac38b0ad1f2b9c6495
 		{"key":"Tax","value":"VAT"},
+		{"key":"State","value":"County"},
 		{"key":"User Information","value":"My Details"},
-		{"key":"Favorites","value":"My Favourites"},
-		{"key":"Favorite","value":"Favourite"},
-		{"key":"Canceled","value":"Cancelled"},
-		{"key":"Center","value":"Centre"}
+		{"key":"ZIP","value":"Postcode"}
 	];
 
 	return { replacements: replacements };
